d95aee7e31ed5a281fb96d139c914cca566fa274
David9191 <jisung9105@gmail.com>
2025-07-05 20:59:49 +0900

    UI: 헤더 UI 1차 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,11 @@ import '../styles/Header.css'
 const Header = () => {
     return (
         <header>
-            <h1>Header</h1>
+            <h1>LIB 청년부 수련회 사업 주문 내역</h1>
+            <div>
+                <div id='total-quantity'>총 수량: {}개</div>
+                <div id='total-sales'>총 매출: {}원</div>
+            </div>
         </header>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../styles/Home.css'
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabase = createClient(
+//   import.meta.env.REACT_APP_SUPABASE_URL,
+//   import.meta.env.REACT_APP_SUPABASE_ANON_KEY
+// )
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div></div>
   )
 }
 
